064b7f3932091cfcfb1d824c1b9bb7156fc8ac9d
pritiranjan-personal <mailtopritiranjan@gmail.com>
2024-06-30 11:31:14 +0530

    delete

--- backend/settings.py
+++ backend/settings.py
@@ -127,10 +127,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_HOST_USER = 'pritiranjan@gmail.com'
-EMAIL_HOST_PASSWORD = 'cvbnm@123'
-EMAIL_USE_TLS = True
